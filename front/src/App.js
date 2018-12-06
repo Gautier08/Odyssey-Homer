0355aa0e7ed4ea9fa1293a92c1aa6a7b24618454
Simonin Gautier <gautier08000@hotmail.com>
2018-12-06 10:31:52 +0100

    input email

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import SignUp from "./SignUp";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SignUp />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
