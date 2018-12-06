2c2c88c9aceae018efb42511387d154537f967cf
Simonin Gautier <gautier08000@hotmail.com>
2018-12-06 11:22:11 +0100

    add input

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,9 +4,16 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: ""
+      email: "",
+      password: "",
+      name: "",
+      lastname: ""
     };
     this.updateEmailField = this.updateEmailField.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.updatePasswordbis = this.updatePasswordbis.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.updateLastname = this.updateLastname.bind(this);
   }
 
   updateEmailField(event) {
@@ -15,11 +22,40 @@ class SignUp extends Component {
     });
   }
 
+  updatePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  updatePasswordbis(event) {
+    this.setState({
+      passwordbis: event.target.value
+    });
+  }
+
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  updateLastname(event) {
+    this.setState({
+      lastname: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.state.email}</h1>
-        <input type="email" name="email" onChange={this.updateEmailField} />
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>{" "}
+        <input onChange={this.updateEmailField} type="email" name="email" />
+        <input onChange={this.updatePassword} type="text" />
+        <input onChange={this.updatePasswordbis} type="text" />
+        <input onChange={this.updateName} type="text" />
+        <input onChange={this.updateLastname} type="text" />
+        <input type="submit" value="Soumettre" />
       </div>
     );
   }
