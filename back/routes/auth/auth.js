22d798edce74b7f06d8b993684350bbd26744904
Simonin Gautier <gautier08000@hotmail.com>
2018-12-10 11:49:28 +0100

    add user in db

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,24 @@
 let express = require("express");
 let router = express.Router();
+const connection = require("../../helpers/db.js");
 
 router.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
+  const formData = req.body;
+  const post = [
+    req.body.email,
+    req.body.password,
+    req.body.name,
+    req.body.lastname
+  ];
+
+  connection.query(
+    "INSERT INTO users(email,password,name,lastname) VALUES(?,?,?,?)",
+    post,
+    (err, results) => {
+      if (err) res.status(500).json({ flash: err.message });
+      else res.status(200).json({ flash: "User has been signed up !" });
+    }
+  );
 });
 
 module.exports = router;
