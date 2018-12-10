45d45e360971b533ee8e65fce3911c06e74ce267
Simonin Gautier <gautier08000@hotmail.com>
2018-12-10 14:21:48 +0100

    add user

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,22 +1,26 @@
-let express = require("express");
-let router = express.Router();
-const connection = require("../../helpers/db.js");
+const express = require("express");
+const router = express.Router();
+const connection = require("../../helpers/db");
 
 router.post("/signup", function(req, res, next) {
-  const formData = req.body;
   const post = [
     req.body.email,
     req.body.password,
     req.body.name,
     req.body.lastname
   ];
-
   connection.query(
     "INSERT INTO users(email,password,name,lastname) VALUES(?,?,?,?)",
     post,
     (err, results) => {
-      if (err) res.status(500).json({ flash: err.message });
-      else res.status(200).json({ flash: "User has been signed up !" });
+      if (err) {
+        console.log(err);
+        res
+          .status(500)
+          .send("La requête ne peut pas être traitée à létait actuel.");
+      } else {
+        res.sendStatus(200);
+      }
     }
   );
 });
