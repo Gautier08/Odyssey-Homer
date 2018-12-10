19704ed2f8ebb6adee2674a5605709835169b3b0
Simonin Gautier <gautier08000@hotmail.com>
2018-12-10 10:28:18 +0100

    add back

---
+++ back/routes/auth/auth.js
@@ -0,0 +1,8 @@
+let express = require("express");
+let router = express.Router();
+
+router.post("/signup", function(req, res, next) {
+  res.send("I am in POST signup");
+});
+
+module.exports = router;
